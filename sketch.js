8b065a1f7e80adb70d4623601b3a31d39ae9f4fe
李一丫 <liyiya@liyiyadeMacBook-Pro.local>
2018-07-06 20:13:59 +0800

    change1

--- sketch.js
+++ sketch.js
@@ -5,63 +5,28 @@
 //Color of the line is the mix of the circle colors
 
 //Initialization function
+let ringNum=5;
+let ringSize=30;
+let size = x*ringSize;
+
 function setup() {
   //Set the size of rendering window - pixels
-  createCanvas(700, 400);
+  createCanvas(500, 500);
 
   //Size of stroke
-  strokeWeight(8);
-
-  //No stroke for shapes
-  noStroke();
-}
+  strokeWight(1);
 
 //Rendering function
 function draw() {
   //Erase all canvas. Set the color to white
-  background(100);
+  background(0);
 
-  strokeWeight(4);
-  stroke(255, 3, 226);
-  line(50, 50, width-50, 50);
+  for (let x= ringNum; x>0; x--)
+  {
+  fill()  
+  ellipse(width/2, height/2,size, size);
 
-  //Top right corner shape position and color
-  stroke(3, 255, 236);
-  line(50, 50, 50, height-50);
-
-  //Bottom left corner shape position and color
-  stroke(0, 255, 0);
-  line(50, height-50, width-50, height-50);
-  //Bottom right corner shape position and color
-  stroke(255, 243, 3);
-  line(width-50, 50,  width-50, height-50);
-
-  stroke(0,255,0);
-  line(50, 50, width-50, height-50);
-
-  stroke(255, 243, 3);
-  line(50, height-50, width-50, 50);
-
-  noStroke();
-
-  //Top left corner shape position and color
-  fill(0, 0, 255);
-  ellipse(50, 50, 100, 100);
-
-  //Top right corner shape position and color
-  fill(255, 0, 0);
-  ellipse(width-50, 50, 100, 100);
-
-  //Bottom left corner shape position and color
-  fill(0, 255, 0);
-  ellipse(50, height-50, 100, 100);
-
-  //Bottom right corner shape position and color
-  fill(255, 255, 0);
-  ellipse(width-50, height-50, 100, 100);
-
-  //hahahahahaha
-
+  }
 
 
 
